cccd3428d48044f15e506597c32b36e04594e009
Artūrs Viļums <arturvilum@gmail.com>
2025-11-30 23:10:08 +0200

    init

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+mod config;
+
+use actix_web::{get, post, patch, App, HttpResponse, HttpServer, Responder};
+
+#[get("/pizzas")]
+async fn get_pizzas() -> impl Responder {
+    HttpResponse::Ok().body("Pizzas available")
+}
+
+#[post("/buy_pizza")]
+async fn buy_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Buying a pizza")
+}
+
+#[patch("/update_pizza/{uuid}")]
+async fn update_pizza() -> impl Responder {
+    HttpResponse::Ok().body("Updated a pizza")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(get_pizzas)
+            .service(buy_pizza)
+            .service(update_pizza)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
